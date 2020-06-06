1843d57eb0992c6bda335babf1cf39055ce5d7cf
Rei-Kigoshi <rei.kigoshi@kellogg.northwestern.edu>
2020-06-06 18:01:04 +0000

    square new is running

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+
 
   # Write your routes above this line
   # =================================
